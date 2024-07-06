73b723baf1fff8c42ff6bc3d9f4e2605550b3fad
Lesya <lesia.str.05@gmail.com>
2024-07-06 14:00:49 +0200

    added the last details

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function checkInputLengthCard(input){
     let value = input.value;
     let digits = '';
 
-    for (let i = 0; i < value.length && digits.length < maxLength; i++){
+    for (let i = 0; i < value.length && digits.length < maxLength && value[i] <= '9'; i++){
         if (value[i] >= '0' && i % 5 === 0){
             digits += ' ';
         }
@@ -56,7 +56,7 @@ function checkInputDataFormat(input){
     let value = input.value;
     let digits = '';
 
-    for (let i = 0; i < value.length && digits.length < maxLength; i++){
+    for (let i = 0; i < value.length && digits.length < maxLength && value[i] <= '9'; i++){
         if (value[i] >= '0' && i % 3 === 0){
            
         }
@@ -72,8 +72,8 @@ function checkInputDataFormatTwo(input){
     
     let value = input.value;
     let digits = '';
-
-    for (let i = 0; i < value.length && digits.length < maxLength; i++){
+   
+    for (let i = 0; i < value.length && digits.length < maxLength && value[i] <= '9'; i++){
         if (value[i] >= '0' && i % 3 === 0){
             // digits += '/';
         }
@@ -108,5 +108,139 @@ function checkLengthName(input){
     const value = input.value;
 if (value.length > maxLength || /[^a-zA-Z]/.test(value)){
     input.value = value.substring(0, maxLength).replace(/[^a-zA-Z]/g, ' ')
+    document.style.display = flex; 
 }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fullInput = document.getElementById("card");
+  
+    const errorMessage = document.getElementById("wrong-card");
+  
+    fullInput.addEventListener("input", function () {
+      if (fullInput.value.trim() === "") {
+        errorMessage.classList.add("wrong-number");
+      } else {
+        errorMessage.style.visibility = "hidden";
+      }
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const fullInput = document.getElementById("cvc");
+  
+    const errorMessage = document.getElementById("error-cvc");
+  
+    fullInput.addEventListener("input", function () {
+      if (fullInput.value.trim() === "") {
+        errorMessage.classList.add("wrong-cvc");
+      } else {
+        errorMessage.style.visibility = "hidden";
+      }
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const fullInput = document.getElementById("exp");
+  
+    const errorMessage = document.getElementById("no-data");
+  
+    fullInput.addEventListener("input", function () {
+      if (fullInput.value.trim() === "") {
+        errorMessage.classList.add("wrong-data");
+      } else {
+        errorMessage.style.visibility = "hidden";
+      }
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const fullInput = document.getElementById("exp2");
+  
+    const errorMessage = document.getElementById("no-data");
+  
+    fullInput.addEventListener("input", function () {
+      if (fullInput.value.trim() === "") {
+        errorMessage.classList.add("wrong-data");
+      } else {
+        errorMessage.style.visibility = "hidden";
+      }
+    });
+  });
+
+
+//   document.getElementById('submit').addEventListener('click', function(){
+
+//     document.getElementById('basic').display = 'none';
+
+//     document.getElementById('submit').classList.add('hidden');
+
+//     document.getElementById('done-message').classList.add('finish');
+//   });
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submit");
+    const containerBase = document.getElementById('basic');
+  
+    const complete = document.getElementById("done-message");
+  
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault();
+        containerBase.style.visibility = "hidden";
+        complete.style.display = "flex";
+    });
+  });
+
+
+
+  document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault();
+    const input1 = document.getElementById('cardholderName').value.trim();
+    const input2 = document.getElementById('card').value;
+    const input3 = document.getElementById('exp').value;
+    const input4 = document.getElementById('exp2').value;
+    const input5 = document.getElementById('cvc').value;
+
+
+    if (input1 && input2 && input3 && input4 && input5) {
+        // document.getElementById('basic').style.visibility = "hidden";
+        document.getElementById('done-message').classList.remove('hidden');
+    } else {
+        alert('Please, fill in all fields.');
+    }
+});
+
+  document.addEventListener("DOMContentLoaded", function () {
+   
+    const submitButton = document.getElementById("continue");
+
+    const containerBase = document.getElementById('basic');
+  
+    const complete = document.getElementById("done-message");
+  
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault();
+        containerBase.style.visibility = "visible";
+        complete.style.visibility = "hidden";
+    });
+  });
+
+
+//   document.getElementById('submit').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     const input1 = document.getElementById('cardholderName').value.trim();
+//     const input2 = document.getElementById('card').value;
+//     const input3 = document.getElementById('exp').value;
+//     const input4 = document.getElementById('exp2').value;
+//     const input5 = document.getElementById('cvc').value;
+
+
+//     if (input1 && input2 && input3 && input4 && input5) {
+//         document.getElementById('basic').style.visibility = "hidden";
+//         document.getElementById('done-message').classList.remove('hidden');
+//     } else {
+//         alert('Будь ласка, заповніть всі поля.');
+//     }
+// });
